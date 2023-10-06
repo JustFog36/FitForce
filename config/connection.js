da7a6d2c007b8137fbac4ecb3f8693036d765dc6
JustFog36 <jfogle36@gmail.com>
2023-10-05 20:45:05 -0400

    corrected connection.js and updated css pathing

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,5 @@
 
 const Sequelize = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
-
+const URI = process.env.MYSQLURI
+const sequelize = new Sequelize(URI);
 module.exports = sequelize;
