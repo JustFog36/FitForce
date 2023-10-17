5796e94e70e896382819f6de598bc277e24d8c16
Sean Finley <finleysean9@gmail.com>
2023-10-16 23:14:32 -0400

    finished index in java and worked on routes

--- Public/assets/java/index.js
+++ Public/assets/java/index.js
@@ -1,54 +1,48 @@
-const loginForm = document.querySelector('#logIn form');
-const signUpForm = document.querySelector('#signUp form');
+const loginForm = document.querySelector('.login-form');
+const signUpForm = document.querySelector('.signUp-form');
 
-loginForm.addEventListener('submit', (event) => {
+const loginFormHelper = async (event) => {
   event.preventDefault(); // prevent the form from submitting
 
-  const email = loginForm.email.value;
-  const password = loginForm.password.value;
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
 
   if (!email || !password) {
     alert('Please enter your email and password.');
     return;
   }
 
-  fetch('/api/authenticate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      username: 'example',
-      password: 'password'
-    })
-  })
-  .then(response => {
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
     if (response.ok) {
-      // If the response is OK, redirect the user to the display page
-      window.location.href = '/display';
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/profile');
     } else {
       // If the response is not OK, display an error message on the page
       const error = new Error(response.statusText);
       error.response = response;
-      throw error;
-    }
-  })
-  .catch(error => {
-    console.error(error);
-    // Display an error message on the page
-    const errorMessage = document.createElement('p');
-    errorMessage.textContent = 'An error occurred while authenticating. Please try again later.';
-    document.body.appendChild(errorMessage);
-  });
+      console.error(error);
+      // Display an error message on the page
+      const errorMessage = document.createElement('p');
+      errorMessage.textContent = 'An error occurred while authenticating. Please try again later.';
+      document.body.appendChild(errorMessage);
+    } 
+  }
+};
 
-});
-
-signUpForm.addEventListener('submit', (event) => {
+const signUpFormHelper =  async (event) => {
   event.preventDefault(); // prevent the form from submitting
 
-  const email = signUpForm.signupEmail.value;
-  const password = signUpForm.signupPassword.value;
-  const confirmPassword = signUpForm.confirmPassword.value;
+  const name = document.querySelector('#signupName').value.trim();
+  const email = document.querySelector('#signupEmail').value.trim();
+  const password = document.querySelector('#signupPassword').value.trim();
+  const confirmPassword = document.querySelector('#confirmPassword').value.trim();
 
   if (!email || !password || !confirmPassword) {
     alert('Please enter your email and password.');
@@ -70,39 +64,34 @@ signUpForm.addEventListener('submit', (event) => {
     return;
   }
 
-  fetch('/api/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      username: 'example',
-      password: 'password'
-    })
-  })
-  .then(response => {
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
     if (response.ok) {
-      // If the response is OK, redirect the user to the dashboard page
-      window.location.href = '/display';
+      document.location.replace('/profile');
     } else {
       // If the response is not OK, display an error message on the page
       const error = new Error(response.statusText);
       error.response = response;
-      throw error;
+      console.error(error);
+      // Display an error message on the page
+      const errorMessage = document.createElement('p');
+      errorMessage.textContent = 'An error occurred while creating your account. Please try again later.';
+      document.body.appendChild(errorMessage);
     }
-  })
-  .catch(error => {
-    console.error(error);
-    // Display an error message on the page
-    const errorMessage = document.createElement('p');
-    errorMessage.textContent = 'An error occurred while creating your account. Please try again later.';
-    document.body.appendChild(errorMessage);
-  });
-
-});
+  }
+};
 
 function isValidEmail(email) {
   // A regular expression to validate email addresses
   const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return emailRegex.test(email);
 }
+
+loginForm.addEventListener('submit', loginFormHelper);
+
+signUpForm.addEventListener('submit', signUpFormHelper);
